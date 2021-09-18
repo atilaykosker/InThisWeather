01301cb85e57a7597601fedac8b40069a5d9e132
atilaykosker <atovarkosker@gmail.com>
2021-09-18 13:12:18 +0300

    change api call method

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+import Head from 'next/head';
+
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
@@ -13,10 +15,18 @@ sagaMiddleware.run(rootSaga);
 function MyApp({ Component, pageProps }) {
    return (
       <Provider store={store}>
+         <Head>
+         <title>InThisWeather | Do not confuse what to wear in rapidly changing weather.
+
+</title>
+
+         </Head>
          <Component {...pageProps} />
       </Provider>
    );
 }
-if (module.hot) { module.hot.accept(MyApp);}
+if (module.hot) {
+   module.hot.accept(MyApp);
+}
 
 export default MyApp;
--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -1,6 +1,6 @@
 const getData = async (city) => {
     const rawData =
-       await fetch(`http://api.weatherapi.com/v1/forecast.json?key=76686ea4e7fd4bb784d92337211508&q=${city}&days=1&aqi=no&alerts=no
+       await fetch(`https://api.weatherapi.com/v1/forecast.json?key=76686ea4e7fd4bb784d92337211508&q=${city}&days=1&aqi=no&alerts=no
      `);
     const data = await rawData.json();
     console.log(data);
