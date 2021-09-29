472235ddd594c247e302535b2cdf321458deb243
atilaykosker <atovarkosker@gmail.com>
2021-09-29 13:22:52 +0300

    ITW is now a PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withPWA = require("next-pwa");
 module.exports = {
    reactStrictMode: true,
 };
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
